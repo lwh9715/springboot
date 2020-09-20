2ef31e52ddf8e1bbb2b05e0d7d04b4ebca37122d
lwx768561 <lwh9715@gmail.com>
2020-09-20 15:27:51 +0800

    springboot security安全机制/rabbitmq/elastsearch/redis

--- springboot-rabbitmq/src/main/java/com/rabbitmq/entity/Book.java
+++ springboot-rabbitmq/src/main/java/com/rabbitmq/entity/Book.java
@@ -1,9 +1,12 @@
 package com.rabbitmq.entity;
 
 
+import lombok.Data;
+
 //使用SpringData的时候，它需要在实体类中设置indexName 和type ，如果和传统型数据库比较的话，就相当于库和表。
 // 需要注意的是indexName和type都必须是小写!!!
 //@Document(indexName = "bookindex" ,type = "book")
+@Data
 public class Book {
 
     private Integer id;
@@ -21,56 +24,5 @@ public class Book {
         this.age = age;
         this.description = description;
         this.createTM = createTM;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCreateTM() {
-        return createTM;
-    }
-
-    public void setCreateTM(String createTM) {
-        this.createTM = createTM;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age='" + age + '\'' +
-                ", description='" + description + '\'' +
-                ", createTM='" + createTM + '\'' +
-                '}';
     }
 }
