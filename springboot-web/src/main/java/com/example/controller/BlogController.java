e2d66f150c6d3cd676ae1f919b46cde881698673
lwx768561 <lwh9715@gmail.com>
2020-09-23 11:00:16 +0800

    添加了springboot-web

---
+++ springboot-web/src/main/java/com/example/controller/BlogController.java
@@ -0,0 +1,36 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/blog")
+public class BlogController {
+
+
+    @RequestMapping("/hello")
+    public String hi(){
+        return "{''message01':'Springboot你好','ajax'}";
+    }
+
+    @RequestMapping("/{id}")
+    public ModelAndView show(@PathVariable("id") Integer id){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("id",id);
+        //相当于return返回页面功能
+        modelAndView.setViewName("blog");
+        return modelAndView;
+    }
+
+    @RequestMapping("/query")
+    public ModelAndView query(@RequestParam(value = "q",required = false)String q){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("q",q);
+        //相当于return返回页面功能
+        modelAndView.setViewName("blog");
+        return modelAndView;
+    }
+}
