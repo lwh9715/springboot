098cb91374514075c4fd3067be49f42447b9af13
lwh9715 <lwh9715@gmail.com>
2021-05-05 09:27:03 +0800

    重新整理提交

--- springboot-jpa/src/main/java/com/jpa/mapper/UserCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jpa.mapper;
-
-import com.jpa.bean.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserCrudRepository extends JpaRepository<User,Integer> {
-}
